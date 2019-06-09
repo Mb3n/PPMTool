af885cb1d4f520c46b7687fe2b5988498c9ff153
Marc Bengsch <mbengsch@me.com>
2019-06-09 12:33:02 +0200

    projectFilled and added Repo

---
+++ PPMToolFullStack/ppmtool/src/main/java/com/marcbengsch/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,13 @@
+package com.marcbengsch.ppmtool.repositories;
+
+
+import com.marcbengsch.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+	@Override
+	Iterable<Project> findAllById(Iterable<Long> iterable);
+}
