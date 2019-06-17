8a081682526fb05ed6dd339cbe783d29efd379ba
Marc Bengsch <mbengsch@me.com>
2019-06-17 18:09:53 +0200

    added Backlog/Project models and their Repos

---
+++ PPMToolFullStack/ppmtool/src/main/java/com/marcbengsch/ppmtool/repositories/ProjectTaskRepository.java
@@ -0,0 +1,10 @@
+package com.marcbengsch.ppmtool.repositories;
+
+
+import com.marcbengsch.ppmtool.domain.ProjectTask;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
+}
