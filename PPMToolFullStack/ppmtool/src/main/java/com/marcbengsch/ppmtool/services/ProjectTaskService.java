fd09eda0686be470e9b4c09913c9bed74ca9016c
Marc Bengsch <mbengsch@me.com>
2019-06-26 11:30:12 +0200

    improved delete operation for backlog/projectTas

--- PPMToolFullStack/ppmtool/src/main/java/com/marcbengsch/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/ppmtool/src/main/java/com/marcbengsch/ppmtool/services/ProjectTaskService.java
@@ -10,8 +10,6 @@ import com.marcbengsch.ppmtool.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class ProjectTaskService {
 
@@ -117,13 +115,7 @@ public class ProjectTaskService {
 
 	public void deletePTByProjectSequence(String backlog_id, String pt_id){
 		ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
-
-
-		Backlog backlog = projectTask.getBacklog();
-		List<ProjectTask> pts = backlog.getProjectTasks();
-		pts.remove(projectTask);
-		backlogRepository.save(backlog);
-
+		
 		projectTaskRepository.delete(projectTask);
 	}
 }
