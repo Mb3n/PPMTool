80e0eb8f32139760f598d960543c4e6e5415b508
Marc Bengsch <mbengsch@me.com>
2019-07-01 15:11:08 +0200

    JWT Provider pre-work

--- PPMToolFullStack/ppmtool/src/main/java/com/marcbengsch/ppmtool/repositories/UserRepository.java
+++ PPMToolFullStack/ppmtool/src/main/java/com/marcbengsch/ppmtool/repositories/UserRepository.java
@@ -1,10 +1,14 @@
 package com.marcbengsch.ppmtool.repositories;
 
-
 import com.marcbengsch.ppmtool.domain.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+
+	User findByUsername(String username);
+	User getById(Long id); // instead of Optional<User> findById(Long id) -> with this new method we get no Optional(User)
+
+
 }
